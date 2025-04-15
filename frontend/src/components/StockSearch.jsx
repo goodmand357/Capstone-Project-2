57fc8cc494121544b539c501854cafd67bba9795
Danisha <goodmand@montclair.edu>
2025-04-15 10:18:56 -0400

    Update StockSearch.jsx

--- frontend/src/components/StockSearch.jsx
+++ frontend/src/components/StockSearch.jsx
@@ -1,1 +1,22 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
+const StockSearch = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState(null);
+
+  const search = async () => {
+    const res = await axios.get(`http://127.0.0.1:5000/search?q=${query}`);
+    setResults(res.data);
+  };
+
+  return (
+    <div>
+      <input value={query} onChange={e => setQuery(e.target.value)} />
+      <button onClick={search}>Search</button>
+      <pre>{JSON.stringify(results, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default StockSearch;
