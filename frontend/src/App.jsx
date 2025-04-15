7600994a646a800fc3ad1932aa283c2ba06510cb
Danisha <goodmand@montclair.edu>
2025-04-15 10:17:39 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import StockSearch from './components/StockSearch';
+import StockPredictor from './components/StockPredictor';
 
+const App = () => {
+  return (
+    <div>
+      <h1>📈 Stock Screener</h1>
+      <StockSearch />
+      <StockPredictor />
+    </div>
+  );
+};
+
+export default App;
